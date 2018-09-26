2b7a554462daeef66ea50b8b8902e9bb0360281d
captain-pool <rickdey1998@gmail.com>
2018-09-26 07:18:08 +0530

    Commit for Assignment-3

--- script.py
+++ script.py
@@ -5,23 +5,24 @@ import os
 import subprocess,shlex
 import time
 df=pd.read_csv("MLCC Assignments.csv")
-df.rename(columns={"Github handle":"github"},inplace=True)
+df.rename(columns={"Github ID":"github"},inplace=True)
 df["github"]=df["github"].apply(lambda x:str(x).split(".com/")[-1].split('/')[0])
 # For this slot of first 86 people
-df2=pd.DataFrame(columns=df.columns)
-errors=[6,74]
-for i in errors:
-    df2=df2.append(df.iloc[i])
-df.drop(errors,inplace=True)
-df.to_csv("eligible.csv")
-df2.to_csv("not-eligible.csv")
-exit()
-os.chdir("Assignment-2")
+#df2=pd.DataFrame(columns=df.columns)
+#errors=[6,74]
+#for i in errors:
+#    df2=df2.append(df.iloc[i])
+#df.drop(errors,inplace=True)
+#df.to_csv("eligible.csv")
+#df2.to_csv("not-eligible.csv")
+os.chdir("Assignment-3")
 #generic branching code
 for account in df["github"]:
     l=[0 for _ in range(6)]
     l[0]=subprocess.Popen(shlex.split("git checkout master")).wait()
     l[1]=subprocess.Popen(shlex.split("git branch {}".format(account))).wait()
+    if l[1]:
+        continue
     l[2]=subprocess.Popen(shlex.split("git checkout {}".format(account))).wait()
     shutil.copy("../base.ipynb","{}.ipynb".format(account))
     l[3]=subprocess.Popen(shlex.split("git add -A")).wait()
